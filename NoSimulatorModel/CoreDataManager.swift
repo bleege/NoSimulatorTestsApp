4a7facb7ec231b7b4550fae2d243d6b9f10d8dd0
Brad Leege <bleege@gmail.com>
2024-11-11 21:55:48 -0600

    Core Data Setup

---
+++ NoSimulatorModel/CoreDataManager.swift
@@ -0,0 +1,16 @@
+//
+//  CoreDataManager.swift
+//  NoSimulatorModel
+//
+//  Created by Brad Leege on 11/11/24.
+//
+
+import Foundation
+
+protocol CoreDataManager {
+    
+}
+
+public class DefaultCoreDataManager: CoreDataManager {
+    
+}
